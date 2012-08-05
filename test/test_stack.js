fd9d3140ba539002efa64aa75cf107fbde4f083a
Scott Nelson <scottbnel@gmail.com>
2012-08-05 17:08:55 -0400

    Initial commit

---
+++ test/test_stack.js
@@ -0,0 +1,87 @@
+var assert = require('assert');
+var Stack = require('../lib/stack');
+
+describe('Stack', function() {
+    var stack;
+
+    beforeEach(function() {
+        stack = new Stack();
+    });
+
+    describe('when adding function', function() {
+        it('returns stack', function() {
+            var result = stack.use(function() {});
+            assert.equal(result, stack);
+        });
+    });
+
+    describe('when handling', function() {
+        it('calls first layer', function(done) {
+            stack.use(function() {
+                done();
+            });
+
+            stack.handle();
+        });
+
+        it('control can be passed to next layer', function(done) {
+            stack.use(function(next) {
+                next();
+            });
+
+            stack.use(function() {
+                done();
+            });
+
+            stack.handle();
+        });
+
+        it('passes errors to accepting layers', function(done) {
+            stack.use(function(next) {
+                next(new Error());
+            });
+
+            stack.use(function(err, next) {
+                assert.ok(err);
+                assert.ok(err instanceof Error);
+                done();
+            });
+
+            stack.handle();
+        });
+
+        it('catches errors thrown in layer', function(done) {
+            stack.use(function() {
+                throw new Error();
+            });
+
+            stack.use(function(err, next) {
+                assert.ok(err);
+                assert.ok(err instanceof Error);
+                done();
+            });
+
+            stack.handle();
+        });
+
+        it('throws unhandled errors', function() {
+            stack.use(function() {
+                throw new Error();
+            });
+
+            assert.throws(function() {
+                stack.handle();
+            });
+        });
+
+        it('handles layers in given context', function(done) {
+            stack.use(function() {
+                assert.equal(this.foo, 'bar');
+                assert.equal(this.baz, 'qux');
+                done();
+            });
+
+            stack.handle({ foo: 'bar', baz: 'qux' });
+        });
+    });
+});
